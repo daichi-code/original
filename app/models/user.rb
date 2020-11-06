f2192499018655aa2d2130fb80edf6bdcc740d67
daichi-code <daichi.pr.daichi@gmail.com>
2020-11-06 16:19:30 +0900

    sns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable, :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
+
 
   has_many :comments
   has_one :address
@@ -19,6 +20,8 @@ class User < ApplicationRecord
 
   has_many :active_notices, class_name: 'Notice', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notices, class_name: 'Notice', foreign_key: 'visited_id', dependent: :destroy
+
+  has_many :sns_credentials
 
   def following?(other_user)
     self.followings.include?(other_user)
@@ -61,20 +64,22 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/
   validates :password, presence: true, length: { minimum: 6, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
-  # def self.from_omniauth(auth)
-  #   sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-  #   # sns認証したことがあればアソシエーションで取得
-  #   # 無ければemailでユーザー検索して取得orビルド(保存はしない)
-  #   user = User.where(email: auth.info.email).first_or_initialize(
-  #     nickname: auth.info.name,
-  #       email: auth.info.email
-  # )
-  #   # userが登録済みであるか判断
-  #   if user.persisted?
-  #     sns.user = user
-  #     sns.save
-  #   end
-  #   { user: user, sns: sns}
-  # end
+
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+  )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns}
+  end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,6 +310,6 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
 
-  # config.omniauth :facebook,ENV['1030650497441491'],ENV['c7d3fbb4cc7972ffa297e77a825a480f']
-  # config.omniauth :google_oauth2,ENV['728780930137-1ao70ouiadtdqtudp66bu5rvsptvqouc.apps.googleusercontent.com'],ENV['MQI8pKIxmU-z6jfu3QmzDH-V']
+  config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
